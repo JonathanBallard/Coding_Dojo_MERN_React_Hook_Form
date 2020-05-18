634b3e4c9d3ee083faf5bf34a120c4750001df9d
Jonathan Ballard <asgardschosen@gmail.com>
2020-05-17 19:30:14 -0700

    fixed state issue!

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -3,7 +3,11 @@ import React, { useState } from 'react';
 import FormField from './FormField'
 
 const Form = props => {
-    const [state, setState] = useState(0);
+    const [state1, setState1] = useState(0);
+    const [state2, setState2] = useState(0);
+    const [state3, setState3] = useState(0);
+    const [state4, setState4] = useState(0);
+    const [state5, setState5] = useState(0);
 
     const field1 = {
         labelName: "first_name",
@@ -47,32 +51,32 @@ const Form = props => {
             <form onSubmit = { e => e.preventDefault() } >
                 <div>
                     <label for={field1.labelName}>{field1.labelValue}</label>
-                    <input name={field1.labelName} type={field1.inputType} onChange= { e => setState({field1: e.target.value}) }></input>
+                    <input name={field1.labelName} type={field1.inputType} onChange= { e => setState1({field1: e.target.value}) }></input>
                 </div>
                 <div>
                     <label for={field2.labelName}>{field2.labelValue}</label>
-                    <input name={field2.labelName} type={field2.inputType} onChange= { e => setState({field2: e.target.value}) } ></input>
+                    <input name={field2.labelName} type={field2.inputType} onChange= { e => setState2({field2: e.target.value}) } ></input>
                 </div>
                 <div>
                     <label for={field3.labelName}>{field3.labelValue}</label>
-                    <input name={field3.labelName} type={field3.inputType} onChange= { e => setState({field3: e.target.value}) } ></input>
+                    <input name={field3.labelName} type={field3.inputType} onChange= { e => setState3({field3: e.target.value}) } ></input>
                 </div>
                 <div>
                     <label for={field4.labelName}>{field4.labelValue}</label>
-                    <input name={field4.labelName} type={field4.inputType} value={state.field4} onChange= { e => setState({field4: e.target.value}) } ></input>
+                    <input name={field4.labelName} type={field4.inputType} value={state4.field4} onChange= { e => setState4({field4: e.target.value}) } ></input>
                 </div>
                 <div>
                     <label for={field5.labelName}>{field5.labelValue}</label>
-                    <input name={field5.labelName} type={field5.inputType} onChange= { e => setState({field5: e.target.value}) } ></input>
+                    <input name={field5.labelName} type={field5.inputType} onChange= { e => setState5({field5: e.target.value}) } ></input>
                 </div>
                 <input type="Submit" value="Submit"></input>
             </form>
             <div>
-                <p>First Name: {state.field1}</p>
-                <p>Last Name: {state.field2}</p>
-                <p>Email: {state.field3}</p>
-                <p>Password: {state.field4}</p>
-                <p>Confirm Password: {state.field5}</p>
+                <p>First Name: {state1.field1}</p>
+                <p>Last Name: {state2.field2}</p>
+                <p>Email: {state3.field3}</p>
+                <p>Password: {state4.field4}</p>
+                <p>Confirm Password: {state5.field5}</p>
             </div>
         </>
     )
